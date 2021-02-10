e47747926b68e164c2ab0d20648427a0b36b44ba
KareemAboAgiza <kemoaboagiza26112000@gmail.com>
2021-02-11 00:08:06 +0200

    Modified 2 Files

--- DoubleLinked.java
+++ DoubleLinked.java
@@ -1,7 +1,20 @@
-//Youssef Ahmed 
-//section 10 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package doublelinked;
 
-package RE.DATA;
+/**
+ *
+ * @author YN
+ */
+
+
+    /**
+     * @param args the command line arguments
+     */
+   
 
 class Node {
 
