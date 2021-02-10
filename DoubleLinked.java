a787c47bbd2ad56aa6c40ca159df2c2e353ad781
KareemAboAgiza <kemoaboagiza26112000@gmail.com>
2021-02-10 23:42:53 +0200

    Added 2 Files

---
+++ DoubleLinked.java
@@ -0,0 +1,115 @@
+//Youssef Ahmed 
+//section 10 
+
+package RE.DATA;
+
+class Node {
+
+    char value;
+    Node next;
+    Node previous;
+
+    Node(char value) {
+        this.value = value;
+    }
+}
+
+class DoubleL {
+
+    Node first = null;
+    Node last = null;
+
+    public void add(int index, char value) {
+        Node newNode = new Node(value);
+        if (index == 0) {
+            if (first == null) {
+                newNode.next = first;
+                first = newNode;
+
+            } else {
+                first.previous = newNode;
+                newNode.next = first;
+                first = newNode;
+
+            }
+            last = newNode;
+
+        } else {
+            Node i = first;
+            for (int count = 0; count < index - 1; count++) {
+                i = i.next;
+            }
+            if (i.next == null) {
+                newNode.next = i.next;
+                i.next = newNode;
+                newNode.previous = i;
+
+            } else {
+                Node j = i.next;
+
+                newNode.next = i.next;
+                i.next = newNode;
+
+                newNode.previous = j.previous;
+                j.previous = newNode;
+            }
+
+        }
+    }
+
+    public void delete(int index) {
+
+        Node i = first;
+
+        if (index == 0) {
+            first.next.previous = null;
+            first = first.next;
+
+        } else {
+            for (int count = 0; count < index - 1; count++) {
+                i = i.next;
+            }
+            if (i.next.next == null) {
+                
+                i.next=i.next.next;
+ 
+                last=i;
+            } else {
+                
+                i.next=i.next.next;
+                i.next.previous=i.next.next.previous;
+            }
+        }
+    }
+
+    public void display() {
+        Node i = first;
+
+        while (i != null) {
+            System.out.print(i.value + "  ");
+
+            i = i.next;
+        }
+        System.out.println("\n");
+    }
+}
+
+public class DoubleLinked {
+
+    public static void main(String[] args) {
+
+        DoubleL l = new DoubleL();
+        l.add(0, 'a');
+        l.add(1, 'b');
+        l.add(2, 'c');
+        l.add(3, 'd');
+        l.add(4, 'e');
+
+        l.display();
+            l.delete(0);
+
+
+        l.display();
+    }
+
+}
---
+++ PriorityQueues.java
@@ -0,0 +1,69 @@
+//Youssef Ahmed 
+//section 10 
+
+package RE.DATA;
+
+class Pqueue
+{
+    int rear  =0 ;//add
+    int front =0 ;//delete
+    int [] arr = new int [30];
+    public void Enqueue(int value)
+    {       
+            int i;
+            int k;
+            for( i=0;i<=rear;i++)
+            {
+                if(value < arr[i])
+                {
+                    break;
+                }
+            }
+            for(  k=rear;k>i;k--)
+            {
+                arr[k]=arr[k-1];
+            }
+            arr[k]=value;
+            rear++;
+            
+        
+ 
+        
+    }
+    public int Dequeue()
+    {
+        return arr[front++];
+    }
+    public int peek()
+    {
+        return arr[front];
+    }
+    public void display()
+    {
+        for(int i= front; i<rear; i++)
+        {
+            System.out.print(arr[i]+"  ");
+        }
+    }
+
+}
+
+public class PriorityQueues {
+
+    public static void main(String[] args) {
+        Pqueue pq= new Pqueue();
+        pq.Enqueue(2);
+        pq.Enqueue(22);
+        pq.Enqueue(1);
+        pq.Enqueue(9);
+        pq.Enqueue(8);
+        pq.Enqueue(7);
+        pq.Enqueue(25);
+        pq.Enqueue(20);
+        pq.Enqueue(0);
+        
+        pq.display();
+
+    }
+
+}
